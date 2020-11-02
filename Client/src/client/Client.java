7a9d72edcef6adfde101a7b92963cb1f8df7becd
kielkow <matheuskiel@fiorifer.com.br>
2020-11-02 16:36:38 -0300

    Set Jokenpo with the server

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,21 +16,51 @@ public class Client {
             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
             
-            while(true) {
-                String msg = "Não preciso de você servidor!";
-
-                output.writeUTF(msg);
-                
-                output.flush();
-
-                msg = input.readUTF();
-                System.out.println("Message from server: " + msg);
-            }
+            Scanner option = new Scanner(System.in);
             
-//            output.close();
-//            input.close();
+            System.out.println("====== Bem-Vindo =====");
+            System.out.println("Selecione uma opcao: ");
+            System.out.println("1 - Pedra");
+            System.out.println("2 - Papel");
+            System.out.println("3 - Tesoura");
+            int optionSelected = option.nextInt();
+            
+            String msg;
+            
+            //Processamento
+            switch (optionSelected) {
+                case 1:
+                    msg = "Pedra";
+                    output.writeUTF(msg);
+                    output.flush();
+                    System.out.println("Cliente selecionou: Pedra");
+                    break;
+                    
+                case 2:
+                    msg = "Papel";
+                    output.writeUTF(msg);
+                    output.flush();
+                    System.out.println("Cliente selecionou: Papel");
+                    break;
+                    
+                case 3:
+                    msg = "Tesoura";
+                    output.writeUTF(msg);
+                    output.flush();
+                    System.out.println("Cliente selecionou: Tesoura");
+                    break;
+                    
+                default:
+                    System.out.println("Opção inválida");
+            }
+        
+            msg = input.readUTF();
+            System.out.println(msg);
+                        
+            output.close();;
+            input.close();
 
-//            socket.close();
+            socket.close();
         } 
         catch (IOException ex) {
             System.out.println("Client error: " + ex);
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -5,6 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 public class Server {
     private ServerSocket serverSocket;
@@ -26,19 +27,76 @@ public class Server {
     private void doConection(Socket socket) throws IOException {
         try {
             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());            
+        
+            String[] array = new String[]{"Pedra","Papel","Tesoura"};
+            String resposta = getRandom(array);
             
-            while (true) {
-                String msg = input.readUTF();
-                System.out.println("Message from client: " + msg);
-            
-                output.writeUTF("Precisa sim!");
-                
-                output.flush();
-            }
-            
-//            output.close();
-//            input.close();
+            String msg = input.readUTF();            
+            System.out.println("Servidor escolheu: " + resposta);
+ 
+            switch (msg) {
+                case "Papel":
+                    if (resposta.equals("Pedra")) {
+                        output.writeUTF("Cliente ganhou!!!!");
+                        output.flush();
+                        System.out.println("Cliente ganhou!!!");
+                    }
+                    else if (resposta.equals("Tesoura")) {
+                        output.writeUTF("Servidor ganhou!!!!");
+                        output.flush();
+                        System.out.println("Servidor ganhou!!!!");
+                    } 
+                    else {
+                        output.writeUTF("Empatou!!!");
+                        output.flush();
+                        System.out.println("Empatou!!!");
+                    }
+                    
+                    break;
+
+                case "Pedra":
+                    if (resposta.equals("Papel")) {
+                        output.writeUTF("Servidor ganhou!!!!");
+                        output.flush();
+                        System.out.println("Servidor ganhou!!!!");
+                    }                  
+                    else if (resposta.equals("Tesoura")) {
+                        output.writeUTF("Cliente ganhou!!!!");
+                        output.flush();
+                        System.out.println("Cliente ganhou!!!!");
+                    } 
+                    else {
+                        output.writeUTF("Empatou!!!");
+                        output.flush();
+                        System.out.println("Empatou!!!");
+                    }
+                    
+                    break;
+
+                case "Tesoura":
+                    if (resposta.equals("Papel")) {
+                        output.writeUTF("Cliente ganhou!!!!");
+                        output.flush();
+                        System.out.println("Cliente ganhou!!!!");
+                    }                   
+                    else if (resposta.equals("Pedra")) {
+                        output.writeUTF("Servidor ganhou!!!!");
+                        output.flush();
+                        System.out.println("Servidor ganhou!!!!");
+                    } 
+                    else {
+                        output.writeUTF("Empatou!!!");
+                        output.flush();
+                        System.out.println("Empatou!!!");
+                    }                    
+                    break;
+
+                default:
+                    System.out.println("Opção invalida");
+            }            
+            output.close();
+            input.close();
         }
         catch(IOException e) {
             System.out.println("Problem to do conection: " + socket.getInetAddress());
@@ -49,6 +107,11 @@ public class Server {
         }
     }
     
+    public static String getRandom(String[] array) {
+        int value = new Random().nextInt(array.length);
+        return array[value];
+    }
+    
     public static void main(String[] args) {
         try {
             Server server = new Server();
@@ -56,12 +119,8 @@ public class Server {
 
             server.createServerSocket(5555);
 
-            Socket socket = server.waitConection();
-            System.out.println("Client conected.");
-            
+            Socket socket = server.waitConection();            
             server.doConection(socket);
-
-            System.out.println("Client desconected.");
         } 
         catch(IOException e) {
             System.out.println(e);
